096a2874aca88de0f5422d07fa2155292523677b
wsteb <wsteb@DESKTOP-OMQ1JLH>
2024-09-05 08:49:06 -0500

    Agregar insumos

---
+++ src/negocios/Insumos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package negocios;
+import java.time.LocalDate;
+import java.util.*;
+
+/**
+ *
+ * @author wsteb
+ */
+public class Insumos {
+    
+    private String nombre;
+    private int cantidad;
+    private String medida; 
+    private int precio_por_unidad;
+    private String codigo;
+    private LocalDate fecha_adquision;
+    private List<String> proveedores;
+    
+    
+}
